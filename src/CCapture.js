8b9c971ebe6e7f539c21a289d4ce83b9b9cb1d26
Panagiotis Panagi <ppanagi@dynopia.com>
2019-02-08 15:26:44 +0200

    Remove gif

--- src/CCapture.js
+++ src/CCapture.js
@@ -3,7 +3,6 @@
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   var Tar = require('./tar.js');
   var download = require('./download.js');
-  var GIF = require('./gif.js');
 }
 
 "use strict";
@@ -438,137 +437,6 @@ CCStreamEncoder.prototype.save = function( callback ) {
 
 }
 
-/*function CCGIFEncoder( settings ) {
-
-	CCFrameEncoder.call( this );
-
-	settings.quality = settings.quality || 6;
-	this.settings = settings;
-
-	this.encoder = new GIFEncoder();
-	this.encoder.setRepeat( 1 );
-  	this.encoder.setDelay( settings.step );
-  	this.encoder.setQuality( 6 );
-  	this.encoder.setTransparent( null );
-  	this.encoder.setSize( 150, 150 );
-
-  	this.canvas = document.createElement( 'canvas' );
-  	this.ctx = this.canvas.getContext( '2d' );
-
-}
-
-CCGIFEncoder.prototype = Object.create( CCFrameEncoder );
-
-CCGIFEncoder.prototype.start = function() {
-
-	this.encoder.start();
-
-}
-
-CCGIFEncoder.prototype.add = function( canvas ) {
-
-	this.canvas.width = canvas.width;
-	this.canvas.height = canvas.height;
-	this.ctx.drawImage( canvas, 0, 0 );
-	this.encoder.addFrame( this.ctx );
-
-	this.encoder.setSize( canvas.width, canvas.height );
-	var readBuffer = new Uint8Array(canvas.width * canvas.height * 4);
-	var context = canvas.getContext( 'webgl' );
-	context.readPixels(0, 0, canvas.width, canvas.height, context.RGBA, context.UNSIGNED_BYTE, readBuffer);
-	this.encoder.addFrame( readBuffer, true );
-
-}
-
-CCGIFEncoder.prototype.stop = function() {
-
-	this.encoder.finish();
-
-}
-
-CCGIFEncoder.prototype.save = function( callback ) {
-
-	var binary_gif = this.encoder.stream().getData();
-
-	var data_url = 'data:image/gif;base64,'+encode64(binary_gif);
-	window.location = data_url;
-	return;
-
-	var blob = new Blob( [ binary_gif ], { type: "octet/stream" } );
-	var url = window.URL.createObjectURL( blob );
-	callback( url );
-
-}*/
-
-function CCGIFEncoder( settings ) {
-
-	CCFrameEncoder.call( this, settings );
-
-	settings.quality = 31 - ( ( settings.quality * 30 / 100 ) || 10 );
-	settings.workers = settings.workers || 4;
-
-	this.extension = '.gif'
-	this.mimeType = 'image/gif'
-
-  	this.canvas = document.createElement( 'canvas' );
-  	this.ctx = this.canvas.getContext( '2d' );
-  	this.sizeSet = false;
-
-  	this.encoder = new GIF({
-		workers: settings.workers,
-		quality: settings.quality,
-		workerScript: settings.workersPath + 'gif.worker.js'
-	} );
-
-    this.encoder.on( 'progress', function( progress ) {
-        if ( this.settings.onProgress ) {
-            this.settings.onProgress( progress )
-        }
-    }.bind( this ) );
-
-    this.encoder.on('finished', function( blob ) {
-        var cb = this.callback;
-        if ( cb ) {
-            this.callback = undefined;
-            cb( blob );
-        }
-    }.bind( this ) );
-
-}
-
-CCGIFEncoder.prototype = Object.create( CCFrameEncoder.prototype );
-
-CCGIFEncoder.prototype.add = function( canvas ) {
-
-	if( !this.sizeSet ) {
-		this.encoder.setOption( 'width',canvas.width );
-		this.encoder.setOption( 'height',canvas.height );
-		this.sizeSet = true;
-	}
-
-	this.canvas.width = canvas.width;
-	this.canvas.height = canvas.height;
-	this.ctx.drawImage( canvas, 0, 0 );
-
-	this.encoder.addFrame( this.ctx, { copy: true, delay: this.settings.step } );
-	this.step();
-
-	/*this.encoder.setSize( canvas.width, canvas.height );
-	var readBuffer = new Uint8Array(canvas.width * canvas.height * 4);
-	var context = canvas.getContext( 'webgl' );
-	context.readPixels(0, 0, canvas.width, canvas.height, context.RGBA, context.UNSIGNED_BYTE, readBuffer);
-	this.encoder.addFrame( readBuffer, true );*/
-
-}
-
-CCGIFEncoder.prototype.save = function( callback ) {
-
-    this.callback = callback;
-
-	this.encoder.render();
-
-}
-
 function CCapture( settings ) {
 
 	var _settings = settings || {},
@@ -618,7 +486,6 @@ function CCapture( settings ) {
 	_log( 'Step is set to ' + _settings.step + 'ms' );
 
     var _encoders = {
-		gif: CCGIFEncoder,
 		webm: CCWebMEncoder,
 		ffmpegserver: CCFFMpegServerEncoder,
 		png: CCPNGEncoder,
